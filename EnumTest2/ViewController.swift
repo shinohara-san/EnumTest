882c9120f08a9b76e3085d59f431ee37cf2bddb2
shinohara-san <someiyoshino0809@gmail.com>
2020-07-07 22:04:40 +0900

    first

--- EnumTest2/ViewController.swift
+++ EnumTest2/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Yuki Shinohara on 2020/07/07.
 //  Copyright © 2020 Yuki Shinohara. All rights reserved.
-//
+//  https://www.youtube.com/watch?v=UjdEItY2Zxc&t=584s
 
 import UIKit
 
@@ -12,9 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    enum MyError: Error{
+        case someErroraaa
+    }
+    
+    func getData(completion: @escaping (Result<Data, Error>)->Void){
+        ///Result型でsuccess/failureの二つのみ
+        let task = URLSession.shared.dataTask(with: URL(string: "")!, completionHandler:  { (data, res, err) in
+            guard let data = data else {
+                completion(.failure(MyError.someErroraaa))
+                return
+            }
+            completion(.success(data))
+        })
+        task.resume()
+    }
 
 }
 
